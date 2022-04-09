748905564967f761c050dc00317c11af21570aa7
Zouhir Bensmaia <freelancebenz@gmail.com>
2022-04-08 22:41:19 -0400

    Modified the entity to be able to receive data from front end

--- src/entities/todo.entity.ts
+++ src/entities/todo.entity.ts
@@ -6,4 +6,15 @@ export class Todo {
     
     @Column()
     title: string;
+
+    @Column()
+    date_dn: Date;
+
+    @Column()
+    date_cr: Date;
+
+    @Column()
+    status: number;
 }
+
+// Status: 1: Null, 2: Requires Attention 3: Priority 4: Soon Finished 5: Done
